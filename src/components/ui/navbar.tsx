d75ac45536924bcfff1bc52cde439f4b85d11a2d
DJJJonas <jonasmelo@duck.com>
2024-04-16 19:00:03 -0300

    add onclick event

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -4,12 +4,15 @@ import { Badge } from "@/components/ui/badge";
 import ItemInfo from "@/interfaces/ItemInfo";
 
 type OnInputType = (e: React.ChangeEvent<HTMLInputElement>) => void;
+type OnClickType = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 
 export default function Navbar({
   OnInput,
+  OnCartClick,
   cart,
 }: {
   readonly OnInput: OnInputType;
+  readonly OnCartClick: OnClickType;
   readonly cart: ItemInfo[];
 }) {
   return (
@@ -20,8 +23,11 @@ export default function Navbar({
         onChange={OnInput}
         className="max-w-screen-sm"
       />
-      <button className="relative rounded-full aspect-square w-10 bg-slate-600 hover:bg-slate-500 active:bg-slate-400">
-        <i className="fa fa-shopping-cart text-slate-200 text-2xl"></i>
+      <button
+        className="relative rounded-full aspect-square w-10 bg-slate-600 hover:bg-slate-500 active:bg-slate-400 text-slate-200 text-2xl"
+        onClick={OnCartClick}
+      >
+        <i className="fa fa-shopping-cart"></i>
         {cart.length ? (
           <Badge className="absolute -left-2 -bottom-2" variant="default">
             {cart.length}
